5d8616f915d6e50a97dc31bd5a5d2f66782e597c
veeveevu <vuhavy0805@gmail.com>
2024-11-07 22:20:20 +0200

    Python exercise

--- Module 11/Exercise 11.1.py
+++ Module 11/Exercise 11.1.py
@@ -1,28 +1,38 @@
 #11.1
 class Publication:
-   def __init__(self, name: str):
+    def __init__(self, publication_type: str, name: str):
        self.name = name
+       self.publication_type = publication_type
+
+    def print_information(self):
+        print(f"Name: {self.name}\n"
+              f"- Type: {self.publication_type}")
 
 class Book(Publication):
     def __init__(self, name: str, author: str, page_count: int):
-        super().__init__(name)
+        super().__init__("Book", name)
         self.author = author
         self.page_count = page_count
 
     def print_information(self):
-        print(f"Book name: {self.name}, author: {self.author}, {self.page_count} pages")
+        super().print_information()
+        print(f"- Author: {self.author}\n"
+              f"- Page: {self.page_count}")
 
 class Magazine(Publication):
     def __init__(self, name: str, chief_editor: str):
-        super().__init__(name)
+        super().__init__("Magazine", name)
         self.chief_editor = chief_editor
 
     def print_information(self):
-        print(f"Magazine name: {self.name}, chief editor: {self.chief_editor}")
+        super().print_information()
+        print(f"- Chief editor: {self.chief_editor}")
 
 if __name__ == "__main__":
     magazine1 = Magazine("Donald Duck", "Aki Hyyppä")
     book1 = Book("Compartment No. 6", "Rosa Liksom", 192)
 
-    magazine1.print_information()
-    book1.print_information()
+    publication_list = [magazine1, book1]
+
+    for item in publication_list:
+        item.print_information()
