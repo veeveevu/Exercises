6c43c07aa9d23d0f79d6c9877c2d1d92c6b5a556
veeveevu <vuhavy0805@gmail.com>
2024-12-01 02:57:00 +0200

    Python exercise

--- Module 13/Exercise 13.1.py
+++ Module 13/Exercise 13.1.py
@@ -1,1 +1,26 @@
 #13.1
+
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+def check_prime_number(num):
+    num = int(num)
+    if num <= 1:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+@app.route('/prime_number/<int:input_number>')
+def prime_number(input_number):
+    result = {
+        "Number" : input_number,
+        "isPrime" : check_prime_number(input_number)
+    }
+    return jsonify(result)
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=5000)
